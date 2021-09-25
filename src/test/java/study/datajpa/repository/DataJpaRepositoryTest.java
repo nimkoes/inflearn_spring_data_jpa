88ebab97d3629c64c900254b25996629e9faf5c2
nimkoes <xxxelppa@gmail.com>
2021-09-25 18:23:07 +0900

    collection type parameter binding (in query)

--- src/test/java/study/datajpa/repository/DataJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/DataJpaRepositoryTest.java
@@ -9,6 +9,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -126,6 +127,24 @@ class DataJpaRepositoryTest {
         List<MemberDto> memberDto = memberRepository.findMemberDto();
         for (MemberDto dto : memberDto) {
             System.out.println("dto = " + dto);
+        }
+    }
+
+    @Test
+    public void findByNames() {
+        Member m1 = new Member();
+        Member m2 = new Member();
+        m1.setUsername("aaa");
+        m1.setAge(10);
+        m2.setUsername("bbb");
+        m2.setAge(20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("aaa", "bbb"));
+        for (Member member : result) {
+            System.out.println("member = " + member);
         }
     }
 }
